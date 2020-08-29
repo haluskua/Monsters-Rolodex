3745e825ad6b5b4430c4e2386c1f73060af7cbcb
haluskua <haluskua.one@gmail.com>
2020-08-30 10:50:27 +1200

    creating API database and displaying users with fetch and json

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: "Hellow Ompa",
+      monsters: [],
     };
   }
+  //setting state with API
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-        <button onClick={() => this.setState({string: 'Hello mangi simbu'})}>Change Text</button>
-        </header>
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}> {monster.name}</h1>
+        ))}
       </div>
     );
   }
