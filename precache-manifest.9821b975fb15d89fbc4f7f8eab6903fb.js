7ed7572f2aea42c22eb62aeac6d1a15e89ca2d4c
haluskua <haluskua.one@gmail.com>
2020-09-02 03:25:03 +1200

    Updates

---
+++ precache-manifest.9821b975fb15d89fbc4f7f8eab6903fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ad60c38716a8e321944611553d11b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b772567a7a0bd20e2d07",
+    "url": "/static/css/main.72daa045.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b772567a7a0bd20e2d07",
+    "url": "/static/js/main.69e2d254.chunk.js"
+  },
+  {
+    "revision": "299bd36d5458990039c2",
+    "url": "/static/js/runtime-main.5fa3ce95.js"
+  }
+]);
